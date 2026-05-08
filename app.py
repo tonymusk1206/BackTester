c9eb102734fcdcd174942752abb9ddd9d2d4eb2f
tonymusk1206 <jhseo1206@gmail.com>
2026-05-08 16:37:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -254,4 +254,5 @@ def api_download():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
